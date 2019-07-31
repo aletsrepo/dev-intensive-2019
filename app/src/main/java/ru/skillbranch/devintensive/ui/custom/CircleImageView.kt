31fc8b2725df3564c1d6f296296f9207f9d12000
Alexey Zakharov <aletzal@yandex-team.ru>
2019-07-31 22:32:37 +0300

    Completed hometask_4

---
+++ app/src/main/java/ru/skillbranch/devintensive/ui/custom/CircleImageView.kt
@@ -0,0 +1,19 @@
+//package ru.skillbranch.devintensive.ui.custom
+//
+//import android.content.Context
+//import android.util.AttributeSet
+//import android.widget.ImageView
+//
+//class CircleImageView @JvmOverloads constructor(
+//    context: Context,
+//    attrs: AttributeSet? = null,
+//    defStyleAttr: Int = 0
+//) : ImageView(context, attrs, defStyleAttr) {
+//
+//    companion object {
+//        private const val DEFAULT_BORDER_COLOR = ""
+//        private const val DEFAULT_BORDER_WIDTH = "2dp"
+//    }
+//
+//
+//}
